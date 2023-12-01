fb15c09a14b317312627d63fc6bcf25d50bda985
ritikarawat22 <152007714+ritikarawat22@users.noreply.github.com>
2023-12-01 13:40:43 +0000

    will be continuing conversions.

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,18 @@
+let score = "true"
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score)
+
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+//conversion result 
+//"33"->33
+//"33abc"-> NaN
+//"undefined"-> NaN
+//true->1   false->0
+//null->0
+
+let isLoggedIn

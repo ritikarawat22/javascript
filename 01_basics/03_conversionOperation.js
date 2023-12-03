817d6e262e30fe2a19aac1f480d47d57c9500441
ritikarawat22 <152007714+ritikarawat22@users.noreply.github.com>
2023-12-03 09:59:30 +0000

    sddfggd

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -32,12 +32,30 @@ console.log(booleanIsLoggedIn);*/
 let booleanIsLoggedIn = Boolean(isLoggedIn)
 console.log(booleanIsLoggedIn);*/
 
-let someNumber = 33
+/*let someNumber = 33
 
 let stringNumber = String(someNumber)
 console.log(stringNumber);
-console.log(typeof stringNumber);
+console.log(typeof stringNumber);*/
 
+// ********************************OPERATIONS **********************************************
+let value = 3
+let negValue = -value
+//console.log(negValue);
 
+//console.log(2+2);
+//console.log(2-2);
+//console.log(2*2);
+//console.log(2**3);
+//console.log(2/3);
+//console.log(2%3);
 
-
+let str1 = "hello"
+let str2 = " ritika"
+let str3 = str1 + str2;
+console.log(str3);
+console.log("1"+2);
+console.log("1"+"2");
+console.log(1+"2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");

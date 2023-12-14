642572144c2f490347fd47d736d6b728bcbb30bb
ritikarawat22 <152007714+ritikarawat22@users.noreply.github.com>
2023-12-14 11:27:36 +0000

    completed

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -53,9 +53,17 @@ let negValue = -value
 let str1 = "hello"
 let str2 = " ritika"
 let str3 = str1 + str2;
-console.log(str3);
-console.log("1"+2);
-console.log("1"+"2");
-console.log(1+"2");
-console.log("1" + 2 + 2);
-console.log(1 + 2 + "2");
+//console.log(str3);
+//console.log("1"+2);
+//console.log("1"+"2");
+//console.log(1+"2");
+//console.log("1" + 2 + 2);
+//console.log(1 + 2 + "2");
+//console.log((3+4)*5%3);
+//console.log(+true);
+//console.log(+"");
+//console.log(+false);
+//console.log(true);
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);

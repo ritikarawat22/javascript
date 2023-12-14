46a4b8d560c00f8c4fa2d95ac61b8b2ae9b562b2
ritikarawat22 <152007714+ritikarawat22@users.noreply.github.com>
2023-12-14 12:07:37 +0000

    learnt comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,25 @@
+//console.log(2>1);
+//console.log(2>=1);
+//console.log(2<1);
+//console.log(2<=1);
+//console.log(2==1);
+//console.log(2!=1);
+
+//console.log("2">1);
+//console.log("02">1);
+
+
+// null can be converted to either nan or 0.
+//console.log(null > 0);
+//console.log(null == 0);
+//console.log(null >= 0);
+
+console.log(undefined > 0);
+console.log(undefined == 0);
+console.log(undefined >= 0);
+console.log(undefined < 0);
+ // note these type of comparison where datatypes of values to be compared are different can create confusion as they convert sometimes 
+ // null to a number 0 or nan.
+
+// (===) is used to check datatype of both the values to be compared. 
+

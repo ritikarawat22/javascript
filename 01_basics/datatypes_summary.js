59a83bd61997aa8965c4b9ee8d6a501e3982919e
ritikarawat22 <152007714+ritikarawat22@users.noreply.github.com>
2023-12-15 05:33:18 +0000

    continue

--- 01_basics/datatypes_summary.js
+++ 01_basics/datatypes_summary.js
@@ -1,9 +1,6 @@
 // #Primitive Data Type(call by value)-means changes r done on copy of value.
 // 7 types: String,Number,Boolean,Null,Undefined,Symbol(to make a value unique),BigInt
 
-// Reference Type Datatype(Non-Primitive)-memory reference can be allocated in this.
-//Arrays,Objects,Functions
-
 // Is javascript dynamic typed or static typed?
 
 const score = 100
@@ -17,4 +14,18 @@ let userEmail; // will have undefined value.
 const id = Symbol('123')
 const anotherId = Symbol('123')
 
-console.log(id === anotherId);
+console.log(id === anotherId)
+
+const bigNumber = 34556768798796764544321n // bigInt
+
+// Reference Type Datatype(Non-Primitive)-memory reference can be allocated in this.
+//Arrays,Objects,Functions
+
+const heroes = ["shaktiman","naagraj","doga"]
+let myObj ={
+    name_1:"Ritika",
+    age: 22,                                   //objects are written inside curly braces as a key value pair.
+}
+
+
+

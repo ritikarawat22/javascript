147c662ecb826852c2865ee3308f0f81847dc8da
ritikarawat22 <152007714+ritikarawat22@users.noreply.github.com>
2023-12-15 18:21:10 +0000

    to be continued

---
+++ 01_basics/05_string.js
@@ -0,0 +1,13 @@
+const name = "hitesh"
+const repoCount = 50
+
+console.log(name + repoCount + "value");
+
+console.log(`hello my name is ${name} and my repocount is ${repoCount}`);
+
+const gameName = new String ('hiteshhc')
+
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
